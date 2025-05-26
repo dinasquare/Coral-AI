e4d039cb81b6dfb206c6449693c4c6de8ba58965
Gauraa <gauraangi25@gmail.com>
2025-05-26 18:34:01 +0530

    Added Chat and Subscription features

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,7 +1,7 @@
 // import { create } from "domain";
 import {integer, pgTable, serial, primaryKey, text, timestamp, varchar, pgEnum } from "drizzle-orm/pg-core"
 
-export const useraSystemEnum = pgEnum('user_system_enum', ['system', 'user']);
+export const userSystemEnum = pgEnum('user_system_enum', ['system', 'user']);
 
 export const chats = pgTable('chats',{
     id: serial('id').primaryKey(),
@@ -12,10 +12,25 @@ export const chats = pgTable('chats',{
     fileKey: text('file_key').notNull(),
 });
 
+export type DrizzleChat = typeof chats.$inferSelect;
+
 export const messages = pgTable('messages',{
     id: serial('id').primaryKey(),
     chatId: integer('chat_id').references(()=>chats.id).notNull(),
     content: text('content').notNull(),
     createdAt: timestamp('created_at').notNull().defaultNow(),
-    role: useraSystemEnum('role').notNull(),
+    role: userSystemEnum('role').notNull(),
+});
+
+export const userSubscriptions = pgTable("user_subscriptions", {
+  id: serial("id").primaryKey(),
+  userId: varchar("user_id", { length: 256 }).notNull().unique(),
+  stripeCustomerId: varchar("stripe_customer_id", { length: 256 })
+    .notNull()
+    .unique(),
+  stripeSubscriptionId: varchar("stripe_subscription_id", {
+    length: 256,
+  }).unique(),
+  stripePriceId: varchar("stripe_price_id", { length: 256 }),
+  stripeCurrentPeriodEnd: timestamp("stripe_current_period_ended_at"),
 });
